6ff87c0c860c56a8e3d7e97184280c480ae08cea
chethanTw <chethandec22@gmail.com>
2014-11-21 14:35:11 +0530

    1254 | Chethan, D3 | Sort drug orders by id

--- ui/app/clinical/controllers/patientDashboardTreatmentController.js
+++ ui/app/clinical/controllers/patientDashboardTreatmentController.js
@@ -5,32 +5,18 @@ angular.module('bahmni.clinical')
             "past": {displayName: "Last Prescription", orders: null}
         };
 
-        var dateCompare = function (drugOrder1, drugOrder2) {
-            return drugOrder1.effectiveStartDate > drugOrder2.effectiveStartDate? -1: 1;
-        };
-
         var getActiveDrugOrders = function() {
             return treatmentService.getActiveDrugOrders($stateParams.patientUuid).then(function (drugOrders) {
-                var prescribedDrugOrders = [];
-                drugOrders.forEach(function(drugOrder){
-                    prescribedDrugOrders.push(Bahmni.Clinical.DrugOrderViewModel.createFromContract(drugOrder))
-                });
-                $scope.drugOrderSections.active.orders = prescribedDrugOrders.sort(dateCompare);
+                $scope.drugOrderSections.active.orders = drugOrders.map(Bahmni.Clinical.DrugOrderViewModel.createFromContract);
             });
         };
 
         var getLastPrescribedDrugOrders = function() {
             var numberOfVisits = $scope.section.numberOfVisits || 1;
             return treatmentService.getPrescribedDrugOrders($stateParams.patientUuid, false, numberOfVisits).then(function (drugOrders) {
-                var prescribedDrugOrders = [];
-                drugOrders.forEach(function(drugOrder){
-                    prescribedDrugOrders.push(Bahmni.Clinical.DrugOrderViewModel.createFromContract(drugOrder))
-                });
-                $scope.drugOrderSections.past.orders = prescribedDrugOrders.sort(dateCompare);
+                $scope.drugOrderSections.past.orders = drugOrders.map(Bahmni.Clinical.DrugOrderViewModel.createFromContract);
             });
         };
-
-
 
         spinner.forPromise($q.all([getActiveDrugOrders(), getLastPrescribedDrugOrders()]));
 
